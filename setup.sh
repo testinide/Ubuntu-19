d9d2a1dc54ec152f941c5852eb35cc730a5a5319
testinide <51829607+testinide@users.noreply.github.com>
2019-06-14 20:56:53 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Ubuntu (GNOME) 19.04 setup script.
+
+dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebia
+
+# Initial Software
+
+sudo apt update
+
+sudo apt install net-tools htop grsync lame asunder soundconverter brasero git mc flatpak \
+openssh-server sshfs gedit-plugin-text-size simplescreenrecorder nano \
+ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks tracker chrome-gnome-shell \
+gnome-tweak-tool qt5-style-plugins spell synaptic -yy
+
+# Add me to any groups I might need to be a part of:
+
+sudo adduser $USER brother
+#ppa
+sudo add-apt-repository ppa:lutris-team/lutris 
+sudo apt-add-repository ppa:dolphin-emu/ppa -yy
+# Remove undesirable packages:
+
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell -yy
+
+# Remove snaps and get packages from apt:
+
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
+gnome-software-plugin-flatpak -yy
+
+#set icons to minimize on click
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+## Remove junk
+sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
+
+## Multimedia
+sudo apt-get install -y gimp
+
+## Games
+sudo apt-get install -y steam-installer
+sudo apt-get install -y lutris
+sudo apt get install -y dolphin-emu-master
+## Disable Apport
+sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
+# set Qt variable in /etc/environment:
+
+sudo bash -c "echo 'QT_QPA_PLATFORMTHEME=gtk2' >> /etc/environment"
+
+# Gotta reboot now:
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** All done! Please reboot now. ***"
