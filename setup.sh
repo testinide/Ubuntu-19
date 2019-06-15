32c56f51441f8e939f33aaed2a68a485a19ee82e
testinide <51829607+testinide@users.noreply.github.com>
2019-06-15 19:23:49 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ gnome-tweak-tool qt5-style-plugins spell synaptic -yy
 
 # Add me to any groups I might need to be a part of:
 
-sudo adduser $USER brother
+sudo adduser $USER other user
 
-#ppa
+#ppa's
 sudo add-apt-repository ppa:lutris-team/lutris 
 sudo apt-add-repository ppa:dolphin-emu/ppa
 sudo add-apt-repository ppa:pcsx2-team/pcsx2-daily
@@ -43,14 +43,16 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
 
 ## Multimedia
-sudo apt-get install -y gimp && kdeconnect
+sudo apt-get install -y gimp 
+sudo apt-get install -y kdeconnect
+sudo apt-get install -y obs-studio
 
 ## Games
 sudo apt-get install -y steam-installer
 sudo apt-get install -y lutris
 sudo apt-get install -y dolphin-emu-master
 sudo apt-get install -y pcsx2 unstable
-sudo apt-get install obs-studio
+
 ## Disable Apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
